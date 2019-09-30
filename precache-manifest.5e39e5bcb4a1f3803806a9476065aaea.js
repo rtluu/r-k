bd1d76bf41d2659deb8396a996359966c9bfddf2
Ryan Luu <ryan@vumble.com>
2019-09-29 21:59:24 -0400

    Updates

---
+++ precache-manifest.5e39e5bcb4a1f3803806a9476065aaea.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f82f83c8e926432aae87b449125f41f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "52d33ae56f83baee42ea",
+    "url": "/static/css/main.b9342738.chunk.css"
+  },
+  {
+    "revision": "7d46d308907fbca99b6e",
+    "url": "/static/js/2.d858c8ec.chunk.js"
+  },
+  {
+    "revision": "52d33ae56f83baee42ea",
+    "url": "/static/js/main.e350ae4b.chunk.js"
+  },
+  {
+    "revision": "2211144055d5089bca25",
+    "url": "/static/js/runtime~main.260cd029.js"
+  },
+  {
+    "revision": "4dcd24018052b2f471b9adf0239898d4",
+    "url": "/static/media/LeslieDawn.4dcd2401.woff"
+  },
+  {
+    "revision": "cb178a68c746c86c95be45fb454a0ce1",
+    "url": "/static/media/bouquet1.cb178a68.png"
+  },
+  {
+    "revision": "ca07b2a5f7d4ae28cb88575acdb8b809",
+    "url": "/static/media/bouquet2.ca07b2a5.png"
+  },
+  {
+    "revision": "ba1f5ecc93b90a40a555565bdd78d555",
+    "url": "/static/media/butterfly-blue.ba1f5ecc.png"
+  },
+  {
+    "revision": "8621e0ce2ae39ee80e2947b36e553843",
+    "url": "/static/media/butterfly-yellow.8621e0ce.png"
+  },
+  {
+    "revision": "effe46b411c6921e01defaab7a725b7b",
+    "url": "/static/media/calendar.effe46b4.svg"
+  },
+  {
+    "revision": "7c14f5caa1ebcac708f91adf16bf8a45",
+    "url": "/static/media/chance.7c14f5ca.png"
+  },
+  {
+    "revision": "7ea009ce1fae4e6f6d73f7b225bb60bf",
+    "url": "/static/media/couple1.7ea009ce.png"
+  },
+  {
+    "revision": "fd67b81b5554c25d48d0998bcc49b271",
+    "url": "/static/media/couple2.fd67b81b.png"
+  },
+  {
+    "revision": "22be50123108b9808c776443a31c54c5",
+    "url": "/static/media/couple3.22be5012.png"
+  },
+  {
+    "revision": "85a977129dcede6a6ff75a45f76c032c",
+    "url": "/static/media/golden-doodle.85a97712.png"
+  },
+  {
+    "revision": "add3a89d254c06e4f27755ad6a49487b",
+    "url": "/static/media/handholding.add3a89d.png"
+  },
+  {
+    "revision": "2e9c242b1f9a7f0e4407fe877eca7acd",
+    "url": "/static/media/home-bottom.2e9c242b.png"
+  },
+  {
+    "revision": "557e2be46401e91008ca4dbad2a2d7b8",
+    "url": "/static/media/home-hero.557e2be4.png"
+  },
+  {
+    "revision": "83fd1720ce464659bff782b3d074da8c",
+    "url": "/static/media/instagram.83fd1720.svg"
+  },
+  {
+    "revision": "381db74ef71cdd52c77ef8e5f74ee403",
+    "url": "/static/media/la.381db74e.png"
+  },
+  {
+    "revision": "b789cf870f20fe49f50443bd248d4558",
+    "url": "/static/media/love1.b789cf87.png"
+  },
+  {
+    "revision": "85f73d6d5e36eae95fcbbedc3c420c43",
+    "url": "/static/media/love2.85f73d6d.png"
+  },
+  {
+    "revision": "cffc084f526f6e782827b44e130b432c",
+    "url": "/static/media/love3.cffc084f.png"
+  },
+  {
+    "revision": "648a13099ddf98e11eafd65920a4d7fe",
+    "url": "/static/media/love4.648a1309.png"
+  },
+  {
+    "revision": "36cb962138529dc4bbf6648372a34049",
+    "url": "/static/media/mail.36cb9621.svg"
+  },
+  {
+    "revision": "c1abc9ce24a4566199a69f8089fd01e1",
+    "url": "/static/media/penguins.c1abc9ce.png"
+  },
+  {
+    "revision": "1d8e700d4a51525f1a77687bd9f16bb0",
+    "url": "/static/media/proposal-pic.1d8e700d.png"
+  },
+  {
+    "revision": "2b0e4aa2367f5881a1627622c5a1e527",
+    "url": "/static/media/ring.2b0e4aa2.png"
+  },
+  {
+    "revision": "d225259715e1962b6f456eec71876044",
+    "url": "/static/media/rose.d2252597.png"
+  },
+  {
+    "revision": "3e7bf978b232c3a5249119ca6ec4cc1b",
+    "url": "/static/media/taro-mochi.3e7bf978.png"
+  },
+  {
+    "revision": "6851a97e4957c90dc0381507393b4293",
+    "url": "/static/media/taro-slush.6851a97e.png"
+  },
+  {
+    "revision": "e0503533c0bc8e065c1a02000eadeff3",
+    "url": "/static/media/townhouse.e0503533.png"
+  }
+]);
